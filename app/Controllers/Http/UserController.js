09c7e65c00b487521c995c477b25d710ad9834e4
Samuel Brehm <samuelbrehm@gmail.com>
2019-06-14 20:30:48 -0300

    Store users success

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Database = use('Database')
+const User = use('App/Models/User')
+
+class UserController {
+  async store ({ request }) {
+    const data = request.only(['username', 'email', 'password', 'type'])
+    const addresses = request.input('addresses')
+
+    data.type = 'client'
+
+    const trx = await Database.beginTransaction()
+
+    const user = await User.create(data, trx)
+    await user.addresses().createMany(addresses, trx)
+
+    await trx.commit()
+
+    return user
+  }
+}
+
+module.exports = UserController
